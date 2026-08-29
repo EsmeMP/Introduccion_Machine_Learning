2ff2874ac03ec2eed3d0fedbd63afb81a660c18f
EsmeMP <esmeralda.padilla.com@gmail.com>
2026-08-28 22:08:57 -0600

    44 Updating Framework GAME

---
+++ notebooks/AiAgents/prueba.py
@@ -0,0 +1,16 @@
+# Modelos de machine learning
+# Modelos supervisados;aprende a traves de datos etiquetados(se le ense;a con ejemplos)
+    # regresion logistica -> predecir probabilidades de clases
+    # arbol de decison -> imita la toma de decisiones de un humano
+    # random forest -> muchos arboles de decisiones
+    # maquinas de soporte vectorial -> dibuja un limite que separa con precion en un plano de caracteristicas
+    # redes neuronales -> entienden patrones no lineales, para tareas > intensas
+# Modelo no supervisado; solo se entregan los datos(datos no etiquetados)
+    # encuentran patrones
+    # clustering->  aprende de datos no etiquetados
+        # k-means->divide los datos en k grupos minimizando la distancia dentro de cada grupo
+        # dbscan -> encuentra grupos basandose en densidad
+    # clusterin jerarquico -> organiza los datos en una estructura de arbol llamada dendograma (inercia, coeficiente de silhoute)
+    # PCA (principal component analystics) -> tecnica de reduccion de dimensionalidad
+# Modelo de refuerzo; se reciben cosas al aprender nuevas cosas(feedback a la IA)
+
